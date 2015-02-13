fec9f6962ed31e74728de4866e41aeb2d88e8a57
Edmund Sumbar <esumbar@gmail.com>
2015-02-13 11:39:46 -0700

    Exercise for module 4 variables and objects

--- studio_game.rb
+++ studio_game.rb
@@ -1,6 +1,28 @@
+current_time = Time.new
+formatted_time = current_time.strftime("on %A %m/%d/%Y at %I:%M%p")
+puts "The game started #{formatted_time}"
+
 name1 = 'larry'
-name2 = 'curly'
-name3 = 'moe'
 health1 = 60
 
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+name2 = 'curly'
+health2 = 125
+
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+health2 = health1
+puts "#{name2.upcase} has a health of #{health2}"
+
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+
+name3 = 'moe'
+health3 = 100
+text = "#{name3.capitalize} has a health of #{health3}."
+puts player3.center(50, '*')
+
+name4 = 'shemp'
+health4 = 90
+puts "#{name4.capitalize.ljust(50,'.')} #{health4} health"
